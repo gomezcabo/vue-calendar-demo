ee97f9fdb2c1a1dd1d23bc2540a3de827af033a4
Juan <gomezcabo@gmail.com>
2018-03-25 14:50:37 +0100

    slides code

---
+++ slides-codes/singlefilecomponents.1.vue
@@ -0,0 +1,23 @@
+<!-- PLANTILLA -->
+<template lang="jade">
+div
+  p {{ greeting }} World
+</template>
+
+<!-- LÓGICA / INSTANCIA -->
+<script>
+module.exports = {
+  data: function () {
+    return {
+      greeting: 'Hello'
+    }
+  }
+}
+</script>
+
+<!-- ESTILOS -->
+<style lang="stylus" scoped>
+p
+  font-size: 2rem;
+  text-align: center;
+</style>
---
+++ slides-codes/singlefilecomponents.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>{{ greeting }} World!</p>
+</template>
+
+<script>
+module.exports = {
+  data: function () {
+    return {
+      greeting: 'Hello'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  p {
+    font-size: 2rem;
+    text-align: center;
+  }
+</style>
+
+
